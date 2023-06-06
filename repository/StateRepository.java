ed7695dc284ed36cf238ea345245388bcc9af2df
forouzan <33863873+feri20@users.noreply.github.com>
2023-06-06 22:50:20 +0330

    Update StateRepository.java

--- repository/StateRepository.java
+++ repository/StateRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface StateRepository extends  CrudRepository<State,Long> , QuerydslPredicateExecutor<State> {
+public interface StateRepository extends JpaRepository<State,Long>,  CrudRepository<State,Long> , QuerydslPredicateExecutor<State> {
     State findFirstByName(String name);
     Optional<State> findById(Long id);
     List<State> findByCountry_Code(String CountryCode);
